d2b86f971755d432f4500525b642aee0a036afbc
Benjamin Blazevic <benjaminblae13@gmail.com>
2019-03-14 00:53:29 +0100

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-flat-50709',
         user: 'admin',
         password: 'test123',
         database: 'smartbrain'
